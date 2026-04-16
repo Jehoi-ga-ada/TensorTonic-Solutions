5a125e0602b928efe7ad2b25766de2c131cc0c36
Jehoiada Wong <121754412+Jehoi-ga-ada@users.noreply.github.com>
2026-04-16 08:06:22 +0700

    Add solution for expected-value-discrete

---
+++ expected-value-discrete/expected-value-discrete.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def expected_value_discrete(x, p):
+    """
+    Returns: float expected value
+    """
+    # Write code here
+    x = np.asarray(x)
+    p = np.asarray(p)
+
+    ev = np.sum(x*p)
+    
+    if np.sum(p) <= 1-1e-7 or np.sum(p) >= 1+1e7:
+        raise ValueError("Probabilites must sum to 1")
+        
+    return ev 
